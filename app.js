502e6415d220cd969bf8bfedefeaaa5acc0df63c
tjdaniel1 <robot2daniel@gmail.com>
2024-04-02 18:38:07 +0900

    Adf

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+var birds = require("./birds");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/page", (req, res) => {
+  res.send("Hello Wordsafld!");
 });
 
 app.listen(port, () => {
