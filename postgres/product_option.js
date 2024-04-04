e0e6adad0479f794ca72927b7e89c741a3751d27
Sujin1517 <11tnwls12@gmail.com>
2024-04-04 15:40:56 +0900

    add product option query

--- postgres/product_option.js
+++ postgres/product_option.js
@@ -1,17 +1,17 @@
-const pg = require("./pg_query_modul");
+const pg = require('./pg_query_modul');
 
-exports.getOptionsByGoodsId = (gds_id) => {
-  return pg.getQuery("", []);
+exports.getAllOptionsByGoodsId = (gds_id) => {
+  return pg.getQuery('SELECT * FROM option WHERE "GDS_ID" = $1', [gds_id]);
 };
 
 exports.getAvailableOptionsByGoodsId = (gds_id) => {
-  return pg.getQuery("", []);
+  return pg.getQuery('SELECT * FROM option WHERE "GDS_ID" = $1 WHERE "EXPSR_YN" = 1', [gds_id]);
 };
 
-exports.updateOptionByOptionId = (option_id) => {
-  return pg.updateQuery("", []);
+exports.updateOptionByOptionId = (props) => {
+  return pg.updateQuery('UPDATE option SET "OPTION_NM" = $1, "OPTION_PRC" = 2$, "SOLDOUT_YN" = $3, "EXPSR_YN" = $4', [...props]);
 };
 
 exports.disalbeOptionByOptionId = (option_id) => {
-  return pg.updateQuery("", []);
+  return pg.updateQuery('UPDATE option SET "USE_YN" = 0 WHERE "OPTION_ID" = $1', [option_id]);
 };
