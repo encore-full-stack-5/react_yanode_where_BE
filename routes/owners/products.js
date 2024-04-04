fe3d1775531e0a00cfa89a2bb63b27cbaee1e70f
parkhyunseo <dazzle1999@naver.com>
2024-04-04 12:38:55 +0900

    feat: fix products and stores functions

--- routes/owners/products.js
+++ routes/owners/products.js
@@ -4,40 +4,22 @@ const router = express.Router();
 const product = require("../../postgres/product");
 
 // owner 상품 관리
-router
-  .route("/:storeId")
-  .get(async (req, res) => {
-    console.log("owner 본인 소유 매장 조회");
-    const result = await product.getProductListBySHOP_ID(req.params.storeId);
-    res.send(result.rows);
-  })
-  .post(async (req, res) => {
-    console.log("owner 상품 추가");
-    const { GDS_NM, GDS_PRC, GDS_DESC, IMG, SOLDOUT_YN, EXPSR_YN } = req.body;
-    const result = await product.createNewProduct([
-      (SHOP_ID = req.params.storeId),
-      GDS_NM,
-      GDS_PRC,
-      GDS_DESC,
-      IMG,
-      SOLDOUT_YN,
-      EXPSR_YN,
-    ]);
-    res.send("상품 추가 성공");
-  });
 
-router.put("/:productId", async (req, res) => {
+router.get("/:storeId", async (req, res) => {
+  console.log("owner 본인 소유 매장 조회");
+  const result = await product.getProductListBySHOP_ID(req.params.storeId);
+  res.send(result.rows);
+});
+
+router.post("/", async (req, res) => {
+  console.log("owner 상품 추가");
+  const result = await product.createNewProduct(req.body.data);
+  res.send("상품 추가 성공");
+});
+
+router.put("/", async (req, res) => {
   console.log("owner 상품 정보 수정");
-  const { GDS_NM, GDS_PRC, GDS_DESC, IMG, SOLDOUT_YN, EXPSR_YN } = req.body;
-  const result = await product.updateProductInfoByGDS_ID([
-    (GDS_ID = req.params.productId),
-    GDS_NM,
-    GDS_PRC,
-    GDS_DESC,
-    IMG,
-    SOLDOUT_YN,
-    EXPSR_YN,
-  ]);
+  const result = await product.updateProductInfoByGDS_ID(req.body.data);
   res.send("상품정보 수정");
 });
 
--- routes/owners/stores.js
+++ routes/owners/stores.js
@@ -4,74 +4,22 @@ const router = express.Router();
 const shop = require("../../postgres/shop");
 
 // owner 상품 관리
-router
-  .route("/:ownerId")
-  .get(async (req, res) => {
-    console.log("owner 본인 소유 매장 조회");
-    const result = await shop.getShopListByOWNER_ID(req.params.ownerId);
-    res.send(result.rows);
-  })
-  .post(async (req, res) => {
-    console.log("owner 매장 추가");
-    const {
-      OWNER_ID,
-      SHOP_NM,
-      CTGRY,
-      ZIPN,
-      BSC_ADDR,
-      DTL_ADDR,
-      SHOP_DESC,
-      OPEN_TIME,
-      CLOSE_TIME,
-      IMG,
-      TELNO,
-    } = req.body;
-    const result = await shop.createNewShop([
-      OWNER_ID,
-      SHOP_NM,
-      CTGRY,
-      ZIPN,
-      BSC_ADDR,
-      DTL_ADDR,
-      SHOP_DESC,
-      OPEN_TIME,
-      CLOSE_TIME,
-      IMG,
-      TELNO,
-    ]);
+router.get("/:ownerId", async (req, res) => {
+  console.log("owner 소유 매장 리스트");
+  const result = await shop.getShopListByOWNER_ID(req.params.ownerId);
+  res.send(result.rows);
+});
 
-    res.send("등록 성공");
-  });
+router.post("/", async (req, res) => {
+  console.log("owner 매장 추가");
+  const result = await shop.createNewShop(req.body.data);
+  res.send("매장 등록 성공");
+});
 
-router.put("/:storeId/:ownerId", async (req, res) => {
+router.put("/", async (req, res) => {
   console.log("owner 매장 정보 수정");
-  const {
-    SHOP_ID,
-    OWNER_ID,
-    SHOP_NM,
-    CTGRY,
-    ZIPN,
-    BSC_ADDR,
-    DTL_ADDR,
-    SHOP_DESC,
-    OPEN_TIME,
-    CLOSE_TIME,
-    IMG,
-  } = req.body;
-  const result = await shop.updateShopInfoAllBySHOP_ID([
-    SHOP_ID,
-    OWNER_ID,
-    SHOP_NM,
-    CTGRY,
-    ZIPN,
-    BSC_ADDR,
-    DTL_ADDR,
-    SHOP_DESC,
-    OPEN_TIME,
-    CLOSE_TIME,
-    IMG,
-  ]);
-  res.send(result.rowCount ? "owner 매장 정보 수정" : "매장 수정 실패");
+  const result = await shop.updateShopInfoAllBySHOP_ID(req.body.data);
+  res.send("owner 매장 정보 수정");
 });
 
 module.exports = router;
