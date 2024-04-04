813fbecb5d2dba6391a49a05148423bcf40466e5
parkhyunseo <dazzle1999@naver.com>
2024-04-04 10:23:46 +0900

    feat: update stores functions

--- routes/owners/stores.js
+++ routes/owners/stores.js
@@ -11,12 +11,67 @@ router
     const result = await shop.getShopListByOWNER_ID(req.params.ownerId);
     res.send(result.rows);
   })
-  .post((req, res) => {
-    res.send("owner 매장 추가");
+  .post(async (req, res) => {
+    console.log("owner 매장 추가");
+    const {
+      OWNER_ID,
+      SHOP_NM,
+      CTGRY,
+      ZIPN,
+      BSC_ADDR,
+      DTL_ADDR,
+      SHOP_DESC,
+      OPEN_TIME,
+      CLOSE_TIME,
+      IMG,
+      TELNO,
+    } = req.body;
+    const result = await shop.createNewShop([
+      OWNER_ID,
+      SHOP_NM,
+      CTGRY,
+      ZIPN,
+      BSC_ADDR,
+      DTL_ADDR,
+      SHOP_DESC,
+      OPEN_TIME,
+      CLOSE_TIME,
+      IMG,
+      TELNO,
+    ]);
+
+    res.send("등록 성공");
   });
 
-router.put("/:productId/:storeId", (req, res) => {
-  res.send("owner 매장 정보 수정");
+router.put("/:storeId/:ownerId", async (req, res) => {
+  console.log("owner 매장 정보 수정");
+  const {
+    SHOP_ID,
+    OWNER_ID,
+    SHOP_NM,
+    CTGRY,
+    ZIPN,
+    BSC_ADDR,
+    DTL_ADDR,
+    SHOP_DESC,
+    OPEN_TIME,
+    CLOSE_TIME,
+    IMG,
+  } = req.body;
+  const result = await shop.updateShopInfoAllBySHOP_ID([
+    SHOP_ID,
+    OWNER_ID,
+    SHOP_NM,
+    CTGRY,
+    ZIPN,
+    BSC_ADDR,
+    DTL_ADDR,
+    SHOP_DESC,
+    OPEN_TIME,
+    CLOSE_TIME,
+    IMG,
+  ]);
+  res.send(result.rowCount ? "owner 매장 정보 수정" : "매장 수정 실패");
 });
 
 module.exports = router;
