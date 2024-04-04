5a68cb884abc04cbc81b50175fc75e3a67b89b1a
parkhyunseo <dazzle1999@naver.com>
2024-04-04 11:35:13 +0900

    feat: update product functions

--- postgres/product.js
+++ postgres/product.js
@@ -1,4 +1,4 @@
-const pg = require('./pg_query_modul');
+const pg = require("./pg_query_modul");
 
 //비번, useyn등 필요없는거 제외하는 수정필요
 /**
@@ -6,9 +6,9 @@ const pg = require('./pg_query_modul');
  * @param {*} gds_id - 상품 ID
  * @returns 해당 `Product`의 정보가 모두 담긴 JSON
  */
-exports.getProductByORD_ID = (gds_id) =>{
-    return pg.getQuery("SELECT * FROM order where \"GDS_ID\" = $1", [gds_id]);
-}
+exports.getProductByORD_ID = (gds_id) => {
+  return pg.getQuery('SELECT * FROM order where "GDS_ID" = $1', [gds_id]);
+};
 
 //비번, useyn등 필요없는거 제외하는 수정필요
 /**
@@ -16,9 +16,9 @@ exports.getProductByORD_ID = (gds_id) =>{
  * @param {*} shop_id - 매장 이 름
  * @returns 해당 `Product`의 리스트가 담긴 JSON List
  */
-exports.getProductListBySHOP_ID = (shop_id) =>{
-    return pg.getQuery('SELECT * FROM product where "SHOP_ID" = $1', [shop_id]);
-}
+exports.getProductListBySHOP_ID = (shop_id) => {
+  return pg.getQuery('SELECT * FROM product where "SHOP_ID" = $1', [shop_id]);
+};
 
 /**
  * `Product`Table에서 매장ID가 일치하는 상품의 목록중,
@@ -26,25 +26,32 @@ exports.getProductListBySHOP_ID = (shop_id) =>{
  * @param {*} shop_id - 매장 이름
  * @returns 해당 `Product`의 리스트가 담긴 JSON List
  */
-exports.getProductBySHOP_IDAndIsEXPSR_YN = (shop_id) =>{
-    return pg.getQuery('SELECT * FROM product where "EXPSR_YN" = 1 and "SHOP_ID"=$1', [shop_id])
-}
+exports.getProductBySHOP_IDAndIsEXPSR_YN = (shop_id) => {
+  return pg.getQuery(
+    'SELECT * FROM product where "EXPSR_YN" = 1 and "SHOP_ID"=$1',
+    [shop_id]
+  );
+};
 
 /**
  * `[SHOP_ID, GDS_NM, GDS_PRC, GDS_DESC, IMG, SOLDOUT_YN, EXPSR_YN]
  * @param {*} props - `Product`생성에 필요한 모든 정보
  * @returns `\<return>.rowcount = 1`
  */
-exports.createNewProduct = (props) =>{
-    return pg.updateQuery(`INSERT INTO product("SHOP_ID","GDS_NM","GDS_PRC","GDS_DESC","IMG","SOLDOUT_YN","EXPSR_YN","USE_YN") values ($1, $2, $3, $4, $5, $6, $7, 1)`, [...props])
-} 
+exports.createNewProduct = (props) => {
+  return pg.updateQuery(
+    `INSERT INTO product("SHOP_ID","GDS_NM","GDS_PRC","GDS_DESC","IMG","SOLDOUT_YN","EXPSR_YN","USE_YN") values ($1, $2, $3, $4, $5, $6, $7, 1)`,
+    [...props]
+  );
+};
 
+exports.updateProductInfoByGDS_ID = (props) => {
+  return pg.updateQuery(
+    `UPDATE product SET  "GDS_NM"= $2,"GDS_PRC" = $3, "GDS_DESC" = $4, "IMG" = $5, "SOLDOUT_YN" = $6, "EXPSR_YN" = $7  WHERE "GDS_ID" = $1`,
+    [...props]
+  );
+};
 
-exports.updateProductInfoByGDS_ID = (props) =>{
-    return pg.updateQuery(`UPDATE product SET  "GDS_NM"= $2, ","GDS_PRC"" = $3, "GDS_DESC" = $4, "IMG" = $5, "SOLDOUT_YN" = $6, "EXPSR_YN" = $7,  WHERE "GDS_ID" = $1`, [...props])
-}
-
-
-exports.updateProductYNByGDS_ID = (props) =>{
-    return pg.updateQuery("", [...props])
-}
+exports.updateProductYNByGDS_ID = (props) => {
+  return pg.updateQuery("", [...props]);
+};
