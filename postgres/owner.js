af3ddce48177ead984016aa6af2313b861d1ae30
parkhyunseo <dazzle1999@naver.com>
2024-04-04 15:37:42 +0900

    feat: update owners functions

--- postgres/owner.js
+++ postgres/owner.js
@@ -1,5 +1,4 @@
-
-const pg = require('./pg_query_modul');
+const pg = require("./pg_query_modul");
 
 //비번, useyn등 필요없는거 제외하는 수정필요
 /**
@@ -8,8 +7,8 @@ const pg = require('./pg_query_modul');
  * @returns 해당 `Owner`의 정보가 모두 담긴 JSON
  */
 exports.OwnerByOWNER_ID = (owner_id) => {
-    return pg.getQuery("SELECT * FROM owner where \"OWNER_ID\" = $1", [owner_id]);
-}
+  return pg.getQuery('SELECT * FROM owner where "OWNER_ID" = $1', [owner_id]);
+};
 
 //비번, useyn등 필요없는거 제외하는 수정필요
 /**
@@ -18,8 +17,8 @@ exports.OwnerByOWNER_ID = (owner_id) => {
  * @returns 해당 `Owner`의 정보가 모두 담긴 JSON
  */
 exports.OwnerByLGN_ID = (lgn_id) => {
-    return pg.getQuery("SELECT * FROM owner where \"LGN_ID\" = $1", [lgn_id]);
-}
+  return pg.getQuery('SELECT * FROM owner where "LGN_ID" = $1', [lgn_id]);
+};
 
 /**
  * `Owner`Table을 받아온 param을 조건으로 `SELECT`한 결과를 반환합니다.
@@ -28,8 +27,11 @@ exports.OwnerByLGN_ID = (lgn_id) => {
  * @returns if correct `\<return>.rowcount = 1`, else `\<return>.rowcount = 0`
  */
 exports.OwnerByLGN_IDAndPASSWD = (lgn_id, passwd) => {
-    return pg.getQuery("SELECT * FROM owner where \"LGN_ID\" = $1 AND \"PASSWD\" = $2", [lgn_id, passwd]);
-}
+  return pg.getQuery(
+    'SELECT * FROM owner where "LGN_ID" = $1 AND "PASSWD" = $2',
+    [lgn_id, passwd]
+  );
+};
 
 /**
  * `[LGN_ID, PASSWD, OWNER_NM, TELNO, ZIPN, BSC_ADDR, DTL_ADDR, GENDER, EMAIL, BRNO]`
@@ -37,8 +39,11 @@ exports.OwnerByLGN_IDAndPASSWD = (lgn_id, passwd) => {
  * @returns `\<return>.rowcount = 1`
  */
 exports.createNewOwner = (props) => {
-    return pg.updateQuery("INSERT INTO owner(\"LGN_ID\", \"PASSWD\", \"OWNER_NM\", \"ZIPN\", \"TELNO\", \"BSC_ADDR\", \"DTL_ADDR\", \"GENDER\", \"EMAIL\", \"BRNO\", \"APRV_STS_CD\", \"JOIN_DT\", \"USE_YN\") values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, 0, now(), 1)", [...props]);
-}
+  return pg.updateQuery(
+    'INSERT INTO owner("LGN_ID", "PASSWD", "OWNER_NM", "ZIPN", "TELNO", "BSC_ADDR", "DTL_ADDR", "GENDER", "EMAIL", "BRNO", "APRV_STS_CD", "JOIN_DT", "USE_YN") values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, 0, now(), 1)',
+    [...props]
+  );
+};
 
 /**
  * `[OWNER_ID, OWNER_NM, TELNO, ZIPN, BSC_ADDR, DTL_ADDR, GENDER, EMAIL, BRNO]`
@@ -46,8 +51,11 @@ exports.createNewOwner = (props) => {
  * @returns `\<return>.rowcount = 1`
  */
 exports.updateOwnerInfoAllByOWNER_ID = (props) => {
-    return pg.updateQuery("UPDATE owner SET OWNER_NM = $2, TELNO = $3, ZIPN = $4, BSC_ADDR = $5, DTL_ADDR = $6, GENDER = $7, EMAIL = $8, BRNO = $9 WHERE OWNER_ID = $1", [...props]);
-}
+  return pg.updateQuery(
+    'UPDATE owner SET "OWNER_NM" = $2, "TELNO" = $3, "ZIPN" = $4, "BSC_ADDR" = $5, "DTL_ADDR" = $6, "GENDER" = $7, "EMAIL" = $8, "BRNO" = $9 WHERE "OWNER_ID" = $1',
+    [...props]
+  );
+};
 
 /**
  * 해당 `Owner`의 `APRV_STS_CD`를 `1`로 변경하고 `APRV_DT`에 현재시각을 기록합니다.
@@ -55,8 +63,11 @@ exports.updateOwnerInfoAllByOWNER_ID = (props) => {
  * @returns `\<return>.rowcount = 1`
  */
 exports.acceptOwnerApproval = (owner_id) => {
-    return pg.updateQuery("UPDATE owner SET APRV_STS_CD = 1, APRV_DT = now() WHERE OWNER_ID = $1", [owner_id]);
-}
+  return pg.updateQuery(
+    "UPDATE owner SET APRV_STS_CD = 1, APRV_DT = now() WHERE OWNER_ID = $1",
+    [owner_id]
+  );
+};
 
 /**
  * 해당 `Owner`의 `USE_YN`을 `0`으로 변경하고 `WHDW_DT`에 현재시각을 기록합니다.
@@ -64,5 +75,8 @@ exports.acceptOwnerApproval = (owner_id) => {
  * @returns `\<return>.rowcount = 1`
  */
 exports.disableOwnerByOWNER_ID = (owner_id) => {
-    return pg.updateQuery("UPDATE owner SET USE_YN = 0, WHDW_DT = now() WHERE OWNER_ID = $1", [owner_id]);
-}
+  return pg.updateQuery(
+    "UPDATE owner SET USE_YN = 0, WHDW_DT = now() WHERE OWNER_ID = $1",
+    [owner_id]
+  );
+};
--- routes/owners/owners.js
+++ routes/owners/owners.js
@@ -1,32 +1,37 @@
 const express = require("express");
 const router = express.Router();
 
+const owner = require("../../postgres/owner");
+
 const ordersRouter = require("./orders");
 const optionsRouter = require("./options");
 const productsRouter = require("./products");
 const storesRouter = require("./stores");
 
 // 계정관리
-router.post("/signup", (req, res) => {
-  res.send("owner 회원가입");
+router.post("/signup", async (req, res) => {
+  console.log("owner 회원가입");
+  const result = await owner.createNewOwner(req.body.data);
+  res.send("회원가입 성공");
 });
 
-router.post("/login", (req, res) => {
-  res.send("owner 로그인");
+// 로그인 미완성
+router.post("/login", async (req, res) => {
+  console.log("owner 로그인");
+  const result = await owner.OwnerByLGN_IDAndPASSWD(lgn_id, passwd);
+  res.send("owner 로그인 성공");
 });
 
-router.post("/logout", (req, res) => {
-  res.send("owner 로그아웃");
+router.get("/profile/:ownerId", async (req, res) => {
+  console.log("owner 정보 조회");
+  // 잘 모르겠음 잘나오긴함
+  const result = await owner.OwnerByLGN_ID(req.params.ownerId);
+  res.json(result.rows[0]); // 조회된 결과 중 첫 번째 행을 반환합니다.
 });
-
-router
-  .route("/profile")
-  .get((req, res) => {
-    res.send("owner 정보 조회");
-  })
-  .put((req, res) => {
-    res.send("owner 정보 수정");
-  });
+router.put("/profile", async (req, res) => {
+  const result = await owner.updateOwnerInfoAllByOWNER_ID(req.body.data);
+  res.send("owner 정보 수정");
+});
 
 router.use("/orders", ordersRouter);
 router.use("/options", optionsRouter);
